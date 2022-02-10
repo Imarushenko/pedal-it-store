bbe2231476932fb9cf0a9b8b19a06218694730e0
Igor Marushenko <igormarushenko@gmail.com>
2022-02-10 12:29:07 +0200

    start creating styles file and continue to work on components files

---
+++ assets/Styles.js
@@ -0,0 +1,17 @@
+// app styles
+// imports
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+
+  gridItem: {
+    flex: 1,
+    margin: 12,
+    height: 125,
+  },
+});
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Styles from "../assets/Styles";
 
 const CategoryGridTile = (props) => {
-    // return (
-    //     <TouchableOpacity style
-    // )
+    return (
+        <TouchableOpacity style
+    )
 }
