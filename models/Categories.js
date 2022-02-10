efb920984d7578d33843d0869ad7f9c38b168e09
Igor Marushenko <igormarushenko@gmail.com>
2022-02-10 09:59:55 +0200

    create models folder and Categories file

---
+++ models/Categories.js
@@ -0,0 +1,9 @@
+// categories class
+class Category {
+  constructor(id, categoryName, imageUrl) {
+    this.id = id;
+    this.categoryName = categoryName;
+    this.imageUrl = imageUrl;
+  }
+  // cxvcxvcxv
+}
