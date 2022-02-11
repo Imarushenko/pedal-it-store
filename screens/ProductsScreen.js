6a4c204fa0ceecac4d8e2e084345c9464bea1e68
Igor Marushenko <igormarushenko@gmail.com>
2022-02-11 15:37:59 +0200

    start products screen creation

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,8 +1,1 @@
-import { PRODUCTS } from "../data/Dummy-Data";
-import styles from "../assets/Styles";
 
-export default function ProductsScreen({ route, navigation }) {
-  const productId = reout.params.productId;
-  const currProduct = PRODUCTS.find((item) => item.id === productId);
-  
-}
